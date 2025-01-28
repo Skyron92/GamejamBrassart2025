87f6279902c73e9e26221586e6b275f2b99643e7
Skyron <courtier.leo@gmail.com>
2025-01-28 22:56:17 +0100

    Player controller (move + jump) + Input Device manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Manages a character controlled by a player
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    // Device management
+    public delegate void DeviceStatusChanged(object sender, PlayerInput playerInput);
+    public event DeviceStatusChanged OnDeviceRemoved;
+    
+    public void OnDeviceLost(PlayerInput playerInput) {
+        OnDeviceRemoved?.Invoke(this, playerInput);
+    }
+    
+    // Move
+    [SerializeField] CharacterController characterController;
+    [SerializeField, Range(0,10)] float moveSpeed = 1;
+    private float MoveSpeed => moveSpeed * Time.deltaTime;
+    private Vector3 _movement = Vector3.zero;
+    
+    public void OnMove(InputAction.CallbackContext context) {
+        _movement.x = context.ReadValue<float>();
+    }
+    
+    // Jump
+    [SerializeField, Range(0,.5f)] float jumpForce = .2f;
+    bool _isJumping;
+    private float _gravity = -9.81f;
+    bool IsGrounded => characterController.isGrounded;
+    
+    public void OnJump(InputAction.CallbackContext context) {
+        if(IsGrounded && context.performed) _isJumping = true;
+    }
+    
+    private void ApplyGravity() {
+        if(IsGrounded && _movement.y < 0) _movement.y = -1f;
+        if (_isJumping) {
+            _movement.y = jumpForce * -2 * _gravity;
+            _isJumping = false;
+        }
+        _movement.y += _gravity * Time.fixedDeltaTime;
+    }
+    
+    private void FixedUpdate() {
+        ApplyGravity();
+        characterController.Move(_movement * MoveSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Manages all the players devices
+/// </summary>
+public class PlayersManager : MonoBehaviour {
+    
+    private int _playerCount;
+    private readonly Dictionary<InputDevice, GameObject> _playersByDevice = new Dictionary<InputDevice, GameObject>();
+
+    public void OnPlayerJoined(PlayerInput playerInput) {
+        _playerCount++;
+        var playerObject = playerInput.gameObject;
+        playerObject.name = "Player " + _playerCount;
+        Debug.Log($"Player {_playerCount} joined using {playerInput.devices[0].displayName}");
+        _playersByDevice.Add(playerInput.devices[0], playerObject);
+        playerObject.GetComponent<PlayerController>().OnDeviceRemoved += OnDeviceLost;
+    }
+
+    void OnDeviceLost(object sender, PlayerInput playerInput) {
+        Debug.Log($"Player {_playerCount} left using {playerInput.devices[0].displayName}");
+        _playerCount--;
+        var device = playerInput.devices[0];
+        Destroy(_playersByDevice[device]);
+        _playersByDevice.Remove(device);
+    }
+}
