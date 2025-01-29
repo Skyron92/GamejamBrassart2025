89ad1eefbaf83a63daca8d65b3a3b54b1ff57ffb
Skyron <courtier.leo@gmail.com>
2025-01-29 15:47:20 +0100

    Gaz spell + background

--- Assets/Scripts/Spells/EnemyTest.cs
+++ Assets/Scripts/Spells/EnemyTest.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
 
 public class EnemyTest : MonoBehaviour, IHitable, IStunnable
 {
@@ -7,14 +9,28 @@ public class EnemyTest : MonoBehaviour, IHitable, IStunnable
         get => health;
         set => health = value < 0 ? 0 : value;
     }
+    
+     [SerializeField] Renderer renderer;
+     private Material _material;
+     private void Awake() {
+         _material = renderer.material;
+     }
 
-    private void OnTriggerEnter(Collider other) {
+     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Spell")) {
             other.gameObject.GetComponentInParent<SpellObject>().Interact(gameObject);
         }
     }
 
-    public void TakeDamage(float damage) {
+    public void TakeDamage(float damage)
+    {
+        float speedAnimation = .1f;
+        var color = _material.color;
+        _material.DOColor(Color.red, speedAnimation).onComplete += () => {
+            _material.DOColor(color, speedAnimation).onComplete += () => _material.DOColor(Color.red, speedAnimation).onComplete += () => {
+                _material.DOColor(color, speedAnimation);
+            };
+        };
         health -= damage;
         if (health <= 0) {
             Die();
---
+++ Assets/Scripts/Spells/GazObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GazObject : SpellObject
+{
+    [SerializeField, Range(1, 10)] private float damage;
+    public override void Interact(GameObject enemy) {
+        var hitable = enemy.GetComponent<IHitable>();
+        hitable.TakeDamage(damage);
+    }
+}
